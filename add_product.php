0b12bcfabfc5712aa9ddd51f675c4db131b225ec
baochuquan <baochuquan@163.com>
2015-08-11 20:48:57 +0800

    分离页头页脚，设计了数据库

---
+++ add_product.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta charset="utf-8">
+		<meta http-equiv="content-type" content="text/html">
+	    <meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>添加产品</title>
+	</head>
+	<body>
+		<!--add product-->
+		<?php
+			require('../../mysql_connect.php')l
+			if($_SERVER['REQUEST_METHOD'] == 'POST')			//handle form
+			{
+				//validate the incoming data
+				$error = array();
+				
+			}
+		?>
+	</body>
+</html>
