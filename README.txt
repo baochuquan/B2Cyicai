16fd4b95f9714a5282ba4a89e1828e297b24301c
baochuquan <baochuquan@163.com>
2015-08-12 22:08:12 +0800

    开始写注册后端代码

---
+++ README.txt
@@ -0,0 +1,4 @@
+This a responsable B2C Web 
+
+The mysqli_connet.php should be in the superior directory;
+
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// This is the main page for the site.
+
+// Include the configuration file:
+require ('includes/config.inc.php'); 
+
+// Set the page title and include the HTML header:
+$page_title = 'Welcome to this Site!';
+include ('includes/head.html');
+
+// Welcome the user (by name if they are logged in):
+echo '<h1>Welcome';
+if (isset($_SESSION['username'])) {
+	echo ", {$_SESSION['username']}";
+}
+else {
+	echo "stranger";
+}
+echo '!</h1>';
+?>
+<p>Spam spam spam spam spam spam
+spam spam spam spam spam spam 
+spam spam spam spam spam spam 
+spam spam spam spam spam spam.</p>
+<p>Spam spam spam spam spam spam
+spam spam spam spam spam spam 
+spam spam spam spam spam spam 
+spam spam spam spam spam spam.</p>
+
+<?php include ('includes/foot.html'); ?>
