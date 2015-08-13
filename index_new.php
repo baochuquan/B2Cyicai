47538b4e968f470e9ec8c5b31e10d5c14b89d63e
baochuquan <baochuquan@163.com>
2015-08-13 22:12:34 +0800

    重新写了header和footer，并在index中加入了传送带

---
+++ index_new.php
@@ -0,0 +1,15 @@
+<?php
+//this is the main page for the site.
+//include the configuration file:
+require ('includes/config.inc.php');
+
+//set the page title and include the HTML header:
+$page_title = '衣彩';
+include ('includes/header.html')
+?>
+		<main role="main">
+			<div class="container">
+				<h1>店主</h1>
+			</div>
+		</main>
+<?php include ('includes/footer.html')  ?>
