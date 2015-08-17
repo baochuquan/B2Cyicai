1446730802b88a6b9c5322355564af70dcd9d92f
baochuquan <baochuquan@163.com>
2015-08-17 20:30:37 +0800

    重构了管理面板的布局，将订单、客户、产品分开，目前完成订单管理的第一项。其中使用了bootstrap的分页

--- management.php
+++ management.php
@@ -13,6 +13,7 @@ include ('includes/header.html')
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-2 sidebar text-center">
+        <h1 class="page-header">管理面板</h1>
 				<ul class="nav nav-sidebar">
 					<li class="active">订单</li>
           <li><a href="#">今日订单</a></li>
@@ -32,17 +33,43 @@ include ('includes/header.html')
     		</ul>
 			</div>
       <div class="col-xs-10 col-xs-offset-2">
-	    	<h1 class="page-header">管理面板</h1>
-        <h2 class="sub-header">Section title</h2>
-        <div class="table-responsive">
+        <h2 class="sub-header">今日订单</h2>
+        <table class="table table-striped">
+          <thead>
+            <tr class="col-name">
+              <th class="baobei">宝贝</th>
+              <th class="price">单价(元)</th>
+              <th class="quantity">数量</th>
+              <th class="amount">实付款(元)</th>
+              <th class="trade-status">
+                <div class="trade-status">
+                  <select id="J_TradeStatusHandle" class="J_NiceSelect" data-config='{"w": 86, "h": 15}'>
+                    <option data-msg="交易状态" value="ALL" >交易状态</option>
+                    <option data-msg="等待买家付款" value="NOT_PAID" >等待买家付款</option>
+                    <option data-msg="买家已付款" value="PAID" >买家已付款</option>
+                    <option data-msg="卖家已发货" value="SEND" >卖家已发货</option>
+                    <option data-msg="交易成功" value="SUCCESS" >交易成功</option>
+                    <option data-msg="交易关闭" value="DROP" >交易关闭</option>
+                    <option data-msg="退款中的订单" value="REFUNDING" >退款中的订单</option>
+                  </select>
+                </div>
+              </th>
+              <th class="trade-operate">交易操作</th>
+            </tr>
+          </thead>
+        </table>
+        <!--<div class="table-responsive">
           <table class="table table-striped">
             <thead>
               <tr>
-                <th>#</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
+                <th>编号</th>
+                <th>买家</th>
+                <th>买家电话</th>
+                <th>收件人</th>
+                <th>收件人地址</th>
+                <th>收件人电话</th>
+                <th>支付状态</th>
+                <th>发件状态</th>
               </tr>
             </thead>
             <tbody>
@@ -76,7 +103,7 @@ include ('includes/header.html')
               </tr>
             </tbody>
           </table>
-  	    </div>
+  	    </div>-->
       </div>
     </div>
 	</div>
