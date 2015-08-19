34faaa689330dde3402acff0801e768b811dd5cb
baochuquan <baochuquan@163.com>
2015-08-19 21:46:04 +0800

    完成了产品页面的设计

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$(documnet).ready(function() {
-  $('.carousel').carousel({
-    interval: 2000
-  });
+$( document ).ready(function() {
+	$('.carousel').carousel({
+    	interval: 2000
+	});
+	
+	$(function(){
+    	var t = $("#text_box");
+    	$("#add").click(function(){        
+     	   t.val(parseInt(t.val())+1)
+    	});
+    	$("#min").click(function(){
+    	    t.val(parseInt(t.val())-1)
+    	});
+	});
 });
--- product.php
+++ product.php
@@ -11,16 +11,52 @@ include ('includes/header.html');
 <main role="main">
 	<div class="container">
 		<div class="row">
+			</div>
+			<!--picture carousel-->
 			<div class="col-xs-6 col-xs-push-3">
-				<div>
+				<!--picture carousel-->
+				<div id="homepage-feature" class="carousel slide mt20">
+					<ol class="carousel-indicators">
+						<li data-target="#homepage-feature" data-slide-to="0" class="active"></li>
+						<li data-target="#homepage-feature" data-slide-to="1"></li>
+						<li data-target="#homepage-feature" data-slide-to="2"></li>
+						<li data-target="#homepage-feature" data-slide-to="3"></li>
+					</ol>
+					<!--Wrappers for slides-->
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="img/okwu.jpg" alt="Product one">
+						</div>
+						<div class="item">	<!--item-->
+							<img src="img/okwu-athletics.jpg" alt="Product two">
+						</div>
+						<div class="item">
+							<img src="img/bartlesvillecf.jpg" alt="Product three">
+						</div>
+						<div class="item">
+							<img src="img/emancipation.jpg" alt="Product four">
+						</div>
+					</div>
+					<!--controls-->
+					<a class="carousel-control left" href="#homepage-feature" data-slide="prev">
+						<span class="icon fa fa-chevron-left"></span>
 
+					<a class="carousel-control right" href="#homepage-feature" data-slide="next">
+						<span class="icon fa fa-chevron-right"></span>
+					</a>
+				</div>	<!--carousel-->
+				<hr class="mt20 mb20"></hr>
+				<div class="row">
+					<div class="col-xs-6 text-center">
+						<button type="button" class="btn btn-warning btn-lg btn-block">立刻购买</button>
+					</div>
+					<div class="col-xs-6 text-center">
+						<button type="button" class="btn btn-danger btn-lg btn-block"><i class="fa fa-shopping-cart"></i> 加入购物车</button>
+					</div>
 				</div>
-				<div>
-
-				<div>
 			</div>
 			<!--the first column-->
-			<div class="col-xs-3 col-xs-pull-6 pr100">
+			<div class="col-xs-3 col-xs-pull-6 pr50">
 				<div class="row ">
 					<div class="col-xs-12 mt20 mb20">
 						<a href="#">
@@ -66,12 +102,132 @@ include ('includes/header.html');
 					</div>
 				</div>
 			</div><!--the first column-->
+			<!--the third column-->
 			<div class="col-xs-3">
-
-			</div>
-		</div>
-		<div class="well well-sm">
-
+				<hr class="mb10"></hr>
+				<div class="row">
+					<div class="col-xs-12">
+						<span class="pull-left" style="font-size:1.2em"><strong>价格:</strong></span>
+						<span class="pull-left ml20" style="font-size:1.2em"><strong>￥100</strong></span>
+					</div>
+					<div class="col-xs-12">
+						<span class="pull-left text-muted">原价:</span>
+						<span class="pull-left text-muted ml20" style="text-decoration:line-through">￥120</span>
+					</div>
+				</div>
+				<hr class="mb10 mt10"></hr>
+				<div class="row">
+					<div class="col-xs-12">
+						<span class="clearfix" style="font-size:1.2em"><strong>尺寸选择</strong></span>
+						<div class="radio">
+						  <label>
+						    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    <div>
+						    	<span class="pr20"><strong>型号:S<strong></span>
+						    	<span class="pr20"><strong>尺码:168<strong></span>
+						    </div>
+						    <div>
+						    	<span class="text-muted pr5"><small>衣长:68</small></span>
+						    	<span class="text-muted pr5"><small>肩宽:40</small></span>
+						    	<span class="text-muted pr5"><small>胸围:89</small></span>
+						    	<span class="text-muted pr5"><small>袖长:17</small></span>
+						    	<span class="text-muted pr5"><small>领围:40.5</small></span>
+						    </div>
+						  </label>
+						</div>
+						<div class="radio">
+						  <label>
+						    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    <div>
+						    	<span class="pr20"><strong>型号:M<strong></span>
+						    	<span class="pr20"><strong>尺码:170<strong></span>
+						    </div>
+						    <div>
+						    	<span class="text-muted pr5"><small>衣长:69.5</small></span>
+						    	<span class="text-muted pr5"><small>肩宽:41.5</small></span>
+						    	<span class="text-muted pr5"><small>胸围:93</small></span>
+						    	<span class="text-muted pr5"><small>袖长:18</small></span>
+						    	<span class="text-muted pr5"><small>领围:40.5</small></span>
+						    </div>
+						  </label>
+						</div>
+						<div class="radio">
+						  <label>
+						    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+						    <div>
+						    	<span class="pr20"><strong>型号:L<strong></span>
+						    	<span class="pr20"><strong>尺码:175<strong></span>
+						    </div>
+						    <div>
+						    	<span class="text-muted pr5"><small>衣长:71</small></span>
+						    	<span class="text-muted pr5"><small>肩宽:43</small></span>
+						    	<span class="text-muted pr5"><small>胸围:98</small></span>
+						    	<span class="text-muted pr5"><small>袖长:19</small></span>
+						    	<span class="text-muted pr5"><small>领围:42</small></span>
+						    </div>
+						  </label>
+						</div>
+						<div class="radio">
+						  <label>
+						    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+						    <div>
+						    	<span class="pr20"><strong>型号:XL<strong></span>
+						    	<span class="pr20"><strong>尺码:180<strong></span>
+						    </div>
+						    <div>
+						    	<span class="text-muted pr5"><small>衣长:72.5</small></span>
+						    	<span class="text-muted pr5"><small>肩宽:44.5</small></span>
+						    	<span class="text-muted pr5"><small>胸围:103</small></span>
+						    	<span class="text-muted pr5"><small>袖长:20</small></span>
+						    	<span class="text-muted pr5"><small>领围:43.5</small></span>
+						    </div>
+						  </label>
+						</div>
+						<div class="radio">
+						  <label>
+						    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+						    <div>
+						    	<span class="pr20"><strong>型号:XXL<strong></span>
+						    	<span class="pr20"><strong>尺码:180<strong></span>
+						    </div>
+						    <div>
+						    	<span class="text-muted pr5"><small>衣长:73.5</small></span>
+						    	<span class="text-muted pr5"><small>肩宽:46</small></span>
+						    	<span class="text-muted pr5"><small>胸围:110</small></span>
+						    	<span class="text-muted pr5"><small>袖长:21</small></span>
+						    	<span class="text-muted pr5"><small>领围:43.5</small></span>
+						    </div>
+						  </label>
+						</div>
+					</div>
+				</div>	
+				<hr class="mb10 mt10"></hr>
+				<div class="row">
+					<div class="col-xs-8">
+						<span class="clearfix" style="font-size:1.2em"><strong>颜色分类</strong></span>
+						<div class="radio">
+							<label class="radio-inline">
+								<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 白色
+							</label>
+							<label class="radio-inline">
+								<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 黑色
+							</label>
+							<label class="radio-inline">
+								<input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> 蓝色
+							</label>
+						</div>
+					</div>
+					<div class="col-xs-4">
+						<span class="clearfix" style="font-size:1.2em"><strong>数量</strong></span>
+						<div class="mt10 mb10">
+							<input id="min" name="" type="button" value="-" />
+							<input id="text_box" name="" type="text" value="1" style="width:25px" />
+							<input id="add" name="" type="button" value="+" />
+						</div>
+					</div>
+				</div>
+				<hr class="mb10 mt10"></hr>
+			</div><!--the third column-->
 		</div>
 	</div>
 </main>
