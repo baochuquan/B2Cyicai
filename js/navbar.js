2ff201b0a8a0d0d945b144a53a4b7909a9537fb0
baochuquan <baochuquan@163.com>
2015-10-21 23:24:56 +0800

    添加了几个页面，包括购物车，订单

--- js/navbar.js
+++ js/navbar.js
@@ -9,12 +9,12 @@ $(function(){
 		var content = '<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="icon fa fa-user fa-lg"></i> ';
 		content += $.cookie(COOKIE_NAME);
 		content += '<span class="caret"></span></a>';
-		content += '<ul class="dropdown-menu"><li><a href="change_password.html">修改密码</a></li><li><a href="myaccount.html">账户设置</a></li><li><a href="#">已买到宝贝</a></li>'
+		content += '<ul class="dropdown-menu"><li><a href="change_password.html">修改密码</a></li><li><a href="myaccount.html">账户设置</a></li><li><a href="myorder.html">我的订单</a></li>'
 		if ($.cookie(COOKIE_LEVEL) == 1) {
 			content +=  '<li role="separator" class="divider"></li><li><a href="order_management.html">订单管理</a></li><li><a href="product_management.html">产品管理</a></li><li><a href="customer_management.html">客户管理</a></li><li role="separator" class="divider"></li>'
 		}
 		content += '<li><a id="logoutlink" href="#">退出账户</a></li></ul></li>'
-		content += '<li><a href="#" title="view cart"><i class="icon fa fa-shopping-cart fa-lg"></i> 购物车</a></li>'
+		content += '<li><a href="shoppingcart.html" title="view cart"><i class="icon fa fa-shopping-cart fa-lg"></i> 购物车</a></li>'
 		$("#cookiedeal li").hide();
 		$("#cookiedeal").append(content);
 	}
--- js/product/addToCart.js
+++ js/product/addToCart.js
@@ -5,6 +5,11 @@
 $(function(){
 	// check if is logged in
 	$("#addtocart").click(function(){
-		alert("hah");
+		if($.cookie("username") && $.cookie("userlevel") && $.cookie("username") != "" && ($.cookie("userlevel")== 0 || $.cookie("userlevel") == 1)){
+			// add a badge after the shopping cart
+		}
+		else {
+			alert("请先登录!");
+		}
 	});
 });
